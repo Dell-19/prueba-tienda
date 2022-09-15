afeca8e32419e260ae33ec12475399746bc248a9
Yarely <yarelyperez628@gmail.com>
2022-09-15 09:07:56 -0500

    commit sin cuerpo

---
+++ AccesoDatosTienda/AccesoProducto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConectarBd;
+
+namespace AccesoDatosTienda
+{
+    public class AccesoProducto
+    {
+        Base b = new Base("localhost","root","","tienda");
+    }
+}
